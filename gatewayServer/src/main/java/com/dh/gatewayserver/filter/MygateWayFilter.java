a5c1caa97addadba41b85d6f4fa849206ed0d52d
all_hao_123 <18380581265@163.com>
2020-08-12 10:30:56 +0800

    'first'

---
+++ gatewayServer/src/main/java/com/dh/gatewayserver/filter/MygateWayFilter.java
@@ -0,0 +1,49 @@
+package com.dh.gatewayserver.filter;
+
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+
+@Component
+public class MygateWayFilter implements GlobalFilter, Ordered {
+
+    private Logger log = LoggerFactory.getLogger(MygateWayFilter.class);
+    /**
+     * 可以用于验证用户登录状态，设置某部分请求信息等。
+     * @param exchange
+     * @param chain
+     * @return
+     */
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        ServerHttpResponse response = exchange.getResponse();
+        String uri = request.getURI().toString();
+        //判断当前是什么类型请求GET请求直接放行
+        if (StringUtils.equals(request.getMethod().toString(), "GET")){
+            return chain.filter(exchange);
+        }
+        //不是GET，判断是不是登录
+        if (uri.contains("login")){
+            return chain.filter(exchange);
+        }
+        String token = request.getHeaders().getFirst("token");
+        System.out.println("token=" + token );
+        return response.setComplete();
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+
+}
---
+++ gatewayServer/src/main/java/com/dh/gatewayserver/filter/TestController.java
@@ -0,0 +1,21 @@
+package com.dh.gatewayserver.filter;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by miyakohiroshi on 2020/7/10.
+ */
+@RequestMapping("test")
+@RestController
+public class TestController {
+
+
+    @GetMapping("test")
+    public String test(String string){
+
+        return "master=====" + string ;
+    }
+
+}
